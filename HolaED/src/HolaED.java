60d3bfb0b8b051c0cd9ecefffcd5f4e69c26a515
Eri <Eri@Eri-pc>
2014-05-11 18:14:41 +0200

    Esta es la rama test

--- HolaED/src/HolaED.java
+++ HolaED/src/HolaED.java
@@ -1,8 +1,11 @@
+import javax.swing.JOptionPane;
+
 
 public class HolaED {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Esta es la rama test");
+		JOptionPane.showConfirmDialog(null, "Esta es la rama test");
 
 	}
 
